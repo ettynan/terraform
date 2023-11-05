bc1b9a29e2607060be8112e1e4845446d749c475
ettynan <ettynan@gmail.com>
2023-11-04 19:11:50 -0700

    updating resource settings in formatting

--- exercises/formatting/format.tf
+++ exercises/formatting/format.tf
@@ -1,6 +1,12 @@
 resource "aws_instance" "ec2" {
   ami           = "ami-09100e341bda441c0"
   instance_type = "t2.micro"
+  root_block_device {
+    encrypted = true
+  }
+  metadata_options {
+    http_tokens = "required"
+  }
 }
 
 #terraform fmt
