2e66d714835f22cd49ae7baae1f48397a6a7c1dd
ettynan <ettynan@gmail.com>
2023-11-15 15:32:25 -0800

    modules exercise

---
+++ exercises/terraform-modules/my-module/ec2.tf
@@ -0,0 +1,6 @@
+# provider "aws" {} # Redundant empty provider block since modules now
+
+resource "aws_instance" "my-ec2" {
+  ami           = "ami-0ad86651279e2c354"
+  instance_type = "t2.micro"
+}
